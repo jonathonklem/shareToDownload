440a80e42a7b8b16d53f400c49e8e52b244b3510
Jonathon Klem <jklem@abbadon.local>
2017-02-17 10:43:32 -0600

    Fix whitespace

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 $(function() {
-	// hide form steps on load
-	$('.step').hide();
+  // hide form steps on load
+  $('.step').hide();
 
-	// function to handle slow scroll
+  // function to handle slow scroll
   $('a[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
@@ -11,67 +11,66 @@ $(function() {
         $('html, body').animate({
           scrollTop: target.offset().top-70
         }, 1000);
-				goToStep(1);
+        goToStep(1);
         return false;
       }
     }
 
   });
 
-	// handle the generation of the javascript the user will need
-	$('.getCode').click(function(e) {
-		// validate all of the fields
-		$("#inputForm").validate({
-		  rules: {
-		    shareUrl: {required: true, url: true},
-				resourceUrl: {required: true, url: true},
-				linkText: {required: true},
-				email: {required: true, email: true}
-		  },
-		  messages: {
-		    shareUrl: "Please enter a valid URL",
-				resourceUrl: "Please enter a valid URL",
-				linkText: "Please enter link text",
-				yourEmail: "Please enter a valid email",
-		  },
-			submitHandler: function(form) {
-				// add thinking indicator
-				goToStep(2);
+  // handle the generation of the javascript the user will need
+  $('.getCode').click(function(e) {
+    // validate all of the fields
+    $("#inputForm").validate({
+      rules: {
+        shareUrl: {required: true, url: true},
+        resourceUrl: {required: true, url: true},
+        linkText: {required: true},
+        email: {required: true, email: true}
+      },
+      messages: {
+        shareUrl: "Please enter a valid URL",
+        resourceUrl: "Please enter a valid URL",
+        linkText: "Please enter link text",
+        yourEmail: "Please enter a valid email",
+      },
+      submitHandler: function(form) {
+        // add thinking indicator
+        goToStep(2);
 
-				// generate their share code
-					// facebook = http://facebook.com/sharer.php?u={{url}}
-				if ($('#socialMedia').val() == "Facebook") {
-					var socialMediaUrl = "http://facebook.com/sharer.php?u=" + $('#shareUrl').val();
-				} else {
-					// twitter = http://twitter.com/intent/tweet?url={{url}}
-					var socialMediaUrl = "http://twitter.com/intent/tweet?url=" + $('#shareUrl').val();
-				}
-				
-				// TODO: how do we handle facebook/twitter?
-				output = "<script>function handleClick" + $('#socialMedia').val() + "() { var wndw=window.open('" + socialMediaUrl+ "');var timer = setInterval(function(){if(wndw.closed){ clearInterval(timer);document.location='" + $('#resourceUrl').val() + "'; }}, 200); }</script>";
-				output += "<a href='#' onclick='handleClick" + $('#socialMedia').val() + "()'>" + $('#linkText').val() + "</a>";
+        // generate their share code
+          // facebook = http://facebook.com/sharer.php?u={{url}}
+        if ($('#socialMedia').val() == "Facebook") {
+          var socialMediaUrl = "http://facebook.com/sharer.php?u=" + $('#shareUrl').val();
+        } else {
+          // twitter = http://twitter.com/intent/tweet?url={{url}}
+          var socialMediaUrl = "http://twitter.com/intent/tweet?url=" + $('#shareUrl').val();
+        }
+        
+        output = "<script>function handleClick" + $('#socialMedia').val() + "() { var wndw=window.open('" + socialMediaUrl+ "');var timer = setInterval(function(){if(wndw.closed){ clearInterval(timer);document.location='" + $('#resourceUrl').val() + "'; }}, 200); }</script>";
+        output += "<a href='#' onclick='handleClick" + $('#socialMedia').val() + "()'>" + $('#linkText').val() + "</a>";
 
-				setTimeout(function() {
-					// display share code	
-					$('#copyMe').val(output);
-					goToStep(3);
-				}, 2000);
-			}
-		});
-	});
+        setTimeout(function() {
+          // display share code 
+          $('#copyMe').val(output);
+          goToStep(3);
+        }, 2000);
+      }
+    });
+  });
 
-	$('.startOver').click(function(e) {
-		e.preventDefault();
-		
-		// clear out previous entries
-		$('#inputForm')[0].reset();
+  $('.startOver').click(function(e) {
+    e.preventDefault();
+    
+    // clear out previous entries
+    $('#inputForm')[0].reset();
 
-		// show the form again
-		goToStep(1);
-	});
+    // show the form again
+    goToStep(1);
+  });
 });
 
 function goToStep(number) {
-	$('.step').hide();
-	$('#step' + number).slideDown();
+  $('.step').hide();
+  $('#step' + number).slideDown();
 }
